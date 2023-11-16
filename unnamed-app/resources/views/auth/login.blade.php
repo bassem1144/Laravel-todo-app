07fd9ac1afb1372285967c7e8d6a0cd69d1f522e
Bassem <bassem.ismail@student.rocva.nl>
2023-11-16 13:34:28 +0100

    added tailwind styling to login & register page

--- unnamed-app/resources/views/auth/login.blade.php
+++ unnamed-app/resources/views/auth/login.blade.php
@@ -1,38 +1,50 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-	<title>Login Page</title>
-	@vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    @vite('resources/css/app.css')
 </head>
 
-<body>
-	<h1>Login</h1>
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
 
-		@if (session()->has('message'))
-            <div>
+    <div class="bg-white p-8 rounded shadow-md w-96">
+        <h1 class="text-2xl font-semibold mb-6">Login</h1>
+
+        @if (session()->has('message'))
+            <div class="bg-green-500 p-4 rounded-lg mb-6 text-white text-center">
                 {{ session('message') }}
             </div>
         @endif
 
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <label for="email" class="block text-sm font-medium text-gray-600 mb-2">Email:</label>
+            <input type="email" id="email" name="email" required
+                class="w-full border rounded-md py-2 px-3 mb-3 @error('email') border-red-500 @enderror">
 
-	<form action="{{ route('login') }}" method="post">
-		@csrf
-		<label for="email">Email:</label>
-		<input type="email" id="email" name="email" required><br><br>
-		<label for="password">Password:</label>
-		<input type="password" id="password" name="password" required><br><br>
+            <label for="password" class="block text-sm font-medium text-gray-600 mb-2">Password:</label>
+            <input type="password" id="password" name="password" required
+                class="w-full border rounded-md py-2 px-3 mb-3 @error('password') border-red-500 @enderror">
 
-		@if ($errors->any())
-			@foreach ($errors->all() as $error)
-				<li>{{ $error }}</li>
-			@endforeach
-		@endif
+            @if ($errors->any())
+                <ul class="text-red-500 mb-4">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
 
-		<input type="submit" value="Login">
-	</form>
-	<br>
-	<a href="{{ route('register') }}">Register</a>
+            <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">Login</button>
+        </form>
+
+        <div class="mt-4 text-sm">
+            <a href="{{ route('register') }}" class="text-blue-500 hover:underline">Register</a>
+        </div>
+    </div>
+
 </body>
 
 </html>
--- unnamed-app/resources/views/auth/register.blade.php
+++ unnamed-app/resources/views/auth/register.blade.php
@@ -1,33 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Login Page</title>
-	@vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Page</title>
+    @vite('resources/css/app.css')
 </head>
 
-<body>
-    <h1>Login</h1>
-    <form action="{{ route('register') }}" method="post">
-        @csrf
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
 
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" value="{{ old('name') }}"><br><br>
+    <div class="bg-white p-8 rounded shadow-md w-96">
+        <h1 class="text-2xl font-semibold mb-6">Register</h1>
 
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" value="{{old('email')}}" ><br><br>
+        <form action="{{ route('register') }}" method="post">
+            @csrf
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" value="{{old('password')}}"><br><br>
+            <label for="name" class="block text-sm font-medium text-gray-600 mb-2">Name:</label>
+            <input type="text" id="name" name="name" value="{{ old('name') }}"
+                class="w-full border rounded-md py-2 px-3 mb-3 @error('name') border-red-500 @enderror">
 
-        @if ($errors->any())
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        @endif
+            <label for="email" class="block text-sm font-medium text-gray-600 mb-2">Email:</label>
+            <input type="email" id="email" name="email" value="{{ old('email') }}"
+                class="w-full border rounded-md py-2 px-3 mb-3 @error('email') border-red-500 @enderror">
 
-        <input type="submit" value="Register">
-    </form>
+            <label for="password" class="block text-sm font-medium text-gray-600 mb-2">Password:</label>
+            <input type="password" id="password" name="password" value="{{ old('password') }}"
+                class="w-full border rounded-md py-2 px-3 mb-3 @error('password') border-red-500 @enderror">
+
+            @if ($errors->any())
+                <ul class="text-red-500 mb-4">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
+
+            <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">Register</button>
+        </form>
+    </div>
+
 </body>
 
 </html>
