3c7884bd9f79da04c9805343052ea4d8a65bd0ae
Bassem <bassem.ismail@student.rocva.nl>
2023-12-05 15:09:57 +0100

    small updates

--- unnamed-app/resources/views/dashboard.blade.php
+++ unnamed-app/resources/views/dashboard.blade.php
@@ -16,34 +16,35 @@
 
    <div class="bg-white rounded-md shadow-md overflow-hidden p-4 mx-auto mt-8 max-w-2xl">
         <table class="min-w-full">
-            <thead>
-                <tr>
-                    <th class="py-2 px-4 border-b">Task</th>
-                    <th class="py-2 px-4 border-b">Description</th>
-                    <th class="py-2 px-4 border-b">Status</th>
-                    <th class="py-2 px-4 border-b">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                    <tr>
-                        <td class="py-2 px-4 border-b">
-                            <a href="{{ route('task.edit', $task->id) }}" class="text-blue-500 hover:underline">
-                                {{ $task->title }}
-                            </a>
-                        </td>
-                        <td class="py-2 px-4 border-b">{{ $task->description }}</td>
-                        <td class="py-2 px-4 border-b">{{ $task->status }}</td>
-                        <td class="py-2 px-4 border-b">
-                            <a href="{{ route('markAsDone', ['GUID' => $task->GUID]) }}" class="text-green-500 hover:text-green-700">
-                                <button class="bg-green-200 text-green-800 py-2 px-4 rounded-full hover:bg-green-300 focus:outline-none focus:shadow-outline-green active:bg-green-400">
-                                    Done
-                                </button>
-                            </a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <thead>
+        <tr>
+            <th class="py-2 px-4 border-b text-left">Task</th>
+            <th class="py-2 px-4 border-b text-left">Description</th>
+            <th class="py-2 px-4 border-b text-left">Status</th>
+            <th class="py-2 px-4 border-b text-center">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($tasks as $task)
+            <tr>
+                <td class="py-2 px-4 border-b">
+                    <a href="{{ route('task.edit', $task->id) }}" class="text-blue-500 hover:underline">
+                        {{ $task->title }}
+                    </a>
+                </td>
+                <td class="py-2 px-4 border-b">{{ $task->description }}</td>
+                <td class="py-2 px-4 border-b">{{ ucwords(str_replace('_', ' ', $task->status)) }}</td>
+                <td class="py-2 px-4 border-b text-center">
+                    <a href="{{ route('markAsDone', ['GUID' => $task->GUID]) }}" class="text-green-500 hover:text-green-700">
+                        <button class="bg-green-200 text-green-800 py-2 px-4 rounded-full hover:bg-green-300 focus:outline-none focus:shadow-outline-green active:bg-green-400">
+                            Done
+                        </button>
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
     </div>
 @endsection
